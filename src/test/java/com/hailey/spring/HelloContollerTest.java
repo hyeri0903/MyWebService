496001b350f5c990428cf586d15b513dc7c1b9b3
hyeri0903 <hyerijung0903@gmail.com>
2021-04-10 06:59:45 +0900

    upload HelloContollerTest and HelloController

---
+++ src/test/java/com/hailey/spring/HelloContollerTest.java
@@ -0,0 +1,30 @@
+package com.hailey.spring;
+
+import com.hailey.spring.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloContollerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk()) //HTTP status 검증 (200)
+                .andExpect(content().string(hello)); //return 값 hello 맞는지 확인
+    }
+}
