c709da2426b7b2fa84ec1c4c65de04d94114a4f2
hyeri0903 <hyerijung0903@gmail.com>
2021-04-11 21:46:56 +0900

    update BaseTimeEntity code

--- src/main/java/com/hailey/spring/domain/posts/Posts.java
+++ src/main/java/com/hailey/spring/domain/posts/Posts.java
@@ -1,6 +1,7 @@
 package com.hailey.spring.domain.posts;
 
 
+import com.hailey.spring.domain.BaseTimeEntity;
 import lombok.NoArgsConstructor;
 
 
@@ -17,7 +18,7 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts  {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
