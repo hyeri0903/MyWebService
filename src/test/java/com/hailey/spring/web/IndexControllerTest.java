8a1b0e0d02b34d30e66f9e2ce2ce685e4206cb72
hyeri0903 <hyerijung0903@gmail.com>
2021-05-03 21:29:02 +0900

    edit application.properties code

--- src/test/java/com/hailey/spring/web/IndexControllerTest.java
+++ src/test/java/com/hailey/spring/web/IndexControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class IndexControllerTest {
@@ -19,11 +18,11 @@ public class IndexControllerTest {
     private TestRestTemplate restTemplate;
 
     @Test
-    public void 메인페이지로_로딩() {
+    public void 메인페이지_로딩() {
         //when
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("스프링 부트로 시작하는 웹서비스");
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
     }
 }
